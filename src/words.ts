f5c6c7754c78235f1780238318f61952ad005914
Lazy Easter Egg <lazyeasteregg@gmail.com>
2023-04-06 23:05:52 -0700

    changed words.ts to be 1 min

--- src/words.ts
+++ src/words.ts
@@ -17,7 +17,7 @@ export function getWordOfTheDay() {
   const now = new Date()
   const start = new Date(2022, 0, 0)
   const diff = Number(now) - Number(start)
-  let day = Math.floor(diff / (1000 * 60 * 5))
+  let day = Math.floor(diff / (1000 * 60 ))
   while (day > answers.length) {
     day -= answers.length
   }
