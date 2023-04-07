1511a5c08eaffbd4dfe7caed6b2ae3953cf88f43
ed chen <echen@starbright.local>
2023-04-06 22:19:19 -0700

    fixed words.tx

--- src/words.ts
+++ src/words.ts
@@ -15,7 +15,7 @@ export function getWordOfTheDay() {
   }
 
   const now = new Date()
-  const start = new Date(2023, 0, 0)
+  const start = new Date(2022, 0, 0)
   const diff = Number(now) - Number(start)
   let day = Math.floor(diff / (1000 * 60 * 5))
   while (day > answers.length) {
